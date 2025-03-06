649707bfb9260d0303ed12bd01f490daaec0f2b8
Joydebroy <104813131+JoydebRoy01@users.noreply.github.com>
2025-03-07 00:54:49 +0530

    Q16

---
+++ Assignment1/randomGen.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+class code {
+    public void generateNumbers() {
+        Random rand = new Random();
+        for (int i = 0; i < 5; i++) {
+            int randomNum = rand.nextInt(100) + 1; 
+            System.out.println(randomNum);
+        }
+    }
+}
+
+public class randomGen{
+    public static void main(String[] args) {
+        code obj = new code();
+        obj.generateNumbers();
+    }
+}
